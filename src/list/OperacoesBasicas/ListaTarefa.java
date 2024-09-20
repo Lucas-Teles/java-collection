df9e22dc4fba08d084e0df485155211a2b978e4a
Lucas Teles <lucasouzateles@gmail.com>
2024-09-20 16:33:47 -0300

    feat: adicionando classes da lista de tarefas

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -1,2 +1,31 @@
-package list.OperacoesBasicas;public class ListaTarefa {
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> listaDeTarefas;
+    public listaDeTarefas() {
+        this.listaDeTarefas = new ArrayList<>();
+    }
+    public void adicionarTarefa(String descricao){
+        listaDeTarefas.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : listaDeTarefas){
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+
+        listaDeTarefas.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalDeTarefas(){
+        return listaDeTarefas.size();
+    }
+    public void obterDescricoesDasTarefas(){
+        System.out.println(listaDeTarefas);
+    }
 }
